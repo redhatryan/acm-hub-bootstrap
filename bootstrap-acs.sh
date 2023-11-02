2f0448dd4b9b518042248556c950df7855de6790
Ryan Etten <Ryan.Etten@innovapost.com>
2023-11-02 09:55:44 -0500

    fixing path

--- bootstrap-acs.sh
+++ bootstrap-acs.sh
@@ -6,7 +6,7 @@ SLEEP_SECONDS=30
 echo ""
 echo "Installing ACS GitOps Cluster."
 
-kustomize build github.com/redhatryan/acs-hub-bootstrap/overlay | oc apply -f -
+kustomize build github.com/redhatryan/acs-hub-bootstrap/bootstrap/overlays | oc apply -f -
 
 echo "Pause $SLEEP_SECONDS seconds for the creation of the GitOps Cluster..."
 sleep $SLEEP_SECONDS
