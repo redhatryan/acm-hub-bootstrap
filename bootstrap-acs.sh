d6ed59969a329fed45436bb757bb85dbb11a4125
Ryan Etten <Ryan.Etten@innovapost.com>
2023-11-02 09:54:01 -0500

    bootstrap acs

---
+++ bootstrap-acs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+LANG=C
+SLEEP_SECONDS=30
+
+echo ""
+echo "Installing ACS GitOps Cluster."
+
+kustomize build github.com/redhatryan/acs-hub-bootstrap/overlay | oc apply -f -
+
+echo "Pause $SLEEP_SECONDS seconds for the creation of the GitOps Cluster..."
+sleep $SLEEP_SECONDS
