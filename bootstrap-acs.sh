2ce6ae4edde752b3a20d6e1362fec69a3ba01932
redhatryan <retten@redhat.com>
2023-11-04 23:01:21 -0500

    fix typo

--- bootstrap-acs.sh
+++ bootstrap-acs.sh
@@ -10,5 +10,5 @@ echo "Installing policies to bootstrap ACS"
 #kustomize build bootstrap/secrets/acs/base | oc apply -f -
 kustomize build bootstrap/policies/overlays/acs --enable-alpha-plugins | oc apply -f -
 
-echo "Labeling cluster with 'gitops: local.local.acs'"
+echo "Labeling cluster with 'gitops: local.acs'"
 oc label managedcluster local-cluster acs=local.acs --overwrite=true
