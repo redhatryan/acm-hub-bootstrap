7021bf88d27440259792c8b5d06492f9a14f716e
redhatryan <retten@redhat.com>
2023-11-05 00:15:30 -0500

    updated script

--- bootstrap-acs.sh
+++ bootstrap-acs.sh
@@ -10,5 +10,8 @@ echo "Installing policies to bootstrap ACS"
 #kustomize build bootstrap/secrets/acs/base | oc apply -f -
 kustomize build bootstrap/policies/overlays/acs --enable-alpha-plugins | oc apply -f -
 
+echo "Join acs-clusters ManagedClusterSet"
+oc label managedcluster acs-hub  cluster.open-cluster-management.io/clusterset=acs-clusters
+
 echo "Labeling cluster with 'gitops: local.acs'"
 oc label managedcluster acs-hub acs-gitops=local.acs --overwrite=true
