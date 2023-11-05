e3663c5df1b35de5b52e9223c0c6561e19550b0c
redhatryan <retten@redhat.com>
2023-11-04 20:17:40 -0500

    comment update

--- bootstrap.sh
+++ bootstrap.sh
@@ -35,7 +35,7 @@ echo "Installing policies and initial secrets"
 kustomize build bootstrap/secrets/base | oc apply -f -
 kustomize build bootstrap/policies/overlays/default --enable-alpha-plugins | oc apply -f -
 
-echo "Labeling cluster with 'gitops: local.home'"
+echo "Labeling cluster with 'gitops: local.hub'"
 oc label managedcluster local-cluster gitops=local.hub --overwrite=true
 
 echo "Check policy compliance with the following command:"
