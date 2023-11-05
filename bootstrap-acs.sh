15b6ffdd0d385341a8f2ea0a43787544de610133
redhatryan <retten@redhat.com>
2023-11-04 22:38:29 -0500

    update tag

--- bootstrap-acs.sh
+++ bootstrap-acs.sh
@@ -10,5 +10,5 @@ echo "Installing policies to bootstrap ACS"
 #kustomize build bootstrap/secrets/acs/base | oc apply -f -
 kustomize build bootstrap/policies/overlays/acs --enable-alpha-plugins | oc apply -f -
 
-echo "Labeling cluster with 'gitops: local.home'"
-oc label managedcluster local-cluster acs=acs.hub --overwrite=true
+echo "Labeling cluster with 'gitops: local.local.acs'"
+oc label managedcluster local-cluster acs=local.acs --overwrite=true
