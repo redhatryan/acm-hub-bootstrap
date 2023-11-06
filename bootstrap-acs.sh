cb90b551d8d7731b4b8ea7b4261176cd55400cbb
redhatryan <retten@redhat.com>
2023-11-05 19:23:47 -0600

    enable secrets

--- bootstrap-acs.sh
+++ bootstrap-acs.sh
@@ -5,14 +5,10 @@ SLEEP_SECONDS=30
 
 echo ""
 
-echo "Installing secrets to bootstrap ACS"
-
-kustomize build bootstrap/policies/overlays/acs/bootstrap --enable-alpha-plugins | oc apply -f -
-
 echo "Installing policies to bootstrap ACS"
 
 #kustomize build bootstrap/secrets/acs/base | oc apply -f -
-kustomize build bootstrap/policies/overlays/acs/base --enable-alpha-plugins | oc apply -f -
+kustomize build bootstrap/policies/overlays/acs --enable-alpha-plugins | oc apply -f -
 
 echo "Join acs-clusters ManagedClusterSet"
 oc label managedcluster acs-hub cluster.open-cluster-management.io/clusterset=acs-clusters --overwrite=true
